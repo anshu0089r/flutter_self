68edfd8a8b19c99db5bef2f0c525378dd7ee68e8
anshu0089r <anshuraj38629@gmail.com>
2026-03-23 23:49:12 +0530

    Border Decoration

--- lib/main.dart
+++ lib/main.dart
@@ -30,53 +30,53 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // var arrname = ["Ram", "Shyam","Raja","Mohan", "Sio", "Dio" ,"Mio"];
+    var arrname = ["Ram", "Shyam","Raja","Mohan", "Sio", "Dio" ,"Mio"];
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blueAccent,
         title: Text(widget.title),
       ),
        body:
-        // ListView.builder(itemBuilder: (context,index){
-        //   return Padding(
-        //     padding: const EdgeInsets.all(8.0),
-        //     child: Text(arrname[index], style: TextStyle(fontSize: 21,fontWeight: FontWeight.w500),),
-        //   );
-        //
-        // },
-        // itemCount: arrname.length,
-        //   itemExtent: 200,
-        //   scrollDirection: Axis.horizontal,
-        // )
-       Center(
-         child: ListView(
-           scrollDirection: Axis.horizontal,
-           reverse: true,
-           children: [
-             Padding(
-               padding: const EdgeInsets.all(8.0),
-               child: Text("one",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
-             ),
-             Padding(
-               padding: const EdgeInsets.all(8.0),
-               child: Text("Two",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
-             ),
-             Padding(
-               padding: const EdgeInsets.all(8.0),
-               child: Text("Three",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
-             ),
-             Padding(
-               padding: const EdgeInsets.all(8.0),
-               child: Text("Four",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
-             ),
-             Padding(
-               padding: const EdgeInsets.all(8.0),
-               child: Text("Five",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
-             )
-           ],
-         ),
+        ListView.builder(itemBuilder: (context,index){
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(arrname[index], style: TextStyle(fontSize: 21,fontWeight: FontWeight.w500),),
+          );
 
-        ));
+        },
+        itemCount: arrname.length,
+          itemExtent: 200,
+          scrollDirection: Axis.horizontal,
+        )
+       // Center(
+       //   child: ListView(
+       //     scrollDirection: Axis.horizontal,
+       //     reverse: true,
+       //     children: [
+       //       Padding(
+       //         padding: const EdgeInsets.all(8.0),
+       //         child: Text("one",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
+       //       ),
+       //       Padding(
+       //         padding: const EdgeInsets.all(8.0),
+       //         child: Text("Two",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
+       //       ),
+       //       Padding(
+       //         padding: const EdgeInsets.all(8.0),
+       //         child: Text("Three",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
+       //       ),
+       //       Padding(
+       //         padding: const EdgeInsets.all(8.0),
+       //         child: Text("Four",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
+       //       ),
+       //       Padding(
+       //         padding: const EdgeInsets.all(8.0),
+       //         child: Text("Five",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
+       //       )
+       //     ],
+       //   ),
+
+        );
 
 
   }
